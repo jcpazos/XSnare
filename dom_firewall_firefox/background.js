d0532645dc88adcd1304ba940087a8cd67be77e4
AppTwistor <jose_pazos8@hotmail.com>
2019-08-22 17:47:45 -0700

    added version without get request for wordpress tests

--- dom_firewall_firefox/background.js
+++ dom_firewall_firefox/background.js
@@ -42,15 +42,15 @@ function mainFrameListener(details) {
   filter.onstop = event => {
     try {
         //TODO: mark verified scripts as 'safe' so contentscript doesn't check them again for dynamic checks
-        var length = str.length;
-        verifyStart = performance.now()
+        //var length = str.length;
+        //verifyStart = performance.now()
         str = verifyHTML(str, details.url, details.tabId);
-        verifyEnd = performance.now();
+        //verifyEnd = performance.now();
 
-        let myRequest = new Request('http://localhost:8000/?time=' + (verifyEnd-verifyStart) + '&loadedSignatures=' + loadedCounter + '&loadedProbes=' + loadedProbes + '&length=' + length + '&url=' + details.url);
+        /*let myRequest = new Request('http://localhost:8000/?time=' + (verifyEnd-verifyStart) + '&loadedSignatures=' + loadedCounter + '&loadedProbes=' + loadedProbes + '&length=' + length + '&url=' + details.url);
         fetch(myRequest).then(function (resp) {
         	//do nothing
-        });
+        });*/
 
     } catch(err) {
         debugger;
